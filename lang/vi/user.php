30450068358eab287f62294ed604fd60ffbb4510
minhlh <hongminhle290@gmail.com>
2024-09-12 21:40:30 +0700

    feat: add locale

---
+++ lang/vi/user.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+return [
+    'password' => 'mật khẩu',
+    'user_name' => 'tên tài khoản',
+    'full_name' => 'họ và tên',
+    'teacher_code' => 'Mã giảng viên',
+    'role_id' => 'vai trò',
+];
