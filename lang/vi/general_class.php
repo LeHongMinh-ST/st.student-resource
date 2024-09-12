de11ec177efbd872363651a2f45abec5b3fa2fb0
minhlh <hongminhle290@gmail.com>
2024-09-12 21:40:30 +0700

    feat: add locale

---
+++ lang/vi/general_class.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+return [
+    'name' => 'Tên lớp học',
+    'code' => 'Mã lớp học',
+    'type' => 'Loại lớp học',
+    'teacher_id' => 'Giáo viên',
+    'status' => 'Trạng thái',
+    'major_id' => 'Ngành học',
+];
