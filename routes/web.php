dd20f502256b23a20c9a207876b72fb8ba48db39
minhlh <hongminhle290@gmail.com>
2024-10-03 00:05:38 +0700

    feat: add pusher

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 declare(strict_types=1);
 
+use App\Events\ImportStudentCourseEvent;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -17,5 +18,10 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     //    event(new ImportStudentCourseEvent('test'));
+    Illuminate\Support\Facades\Log::info('minh');
+    event(new ImportStudentCourseEvent(
+        message: 'success',
+        userId: 1
+    ));
     return view('welcome');
 });
