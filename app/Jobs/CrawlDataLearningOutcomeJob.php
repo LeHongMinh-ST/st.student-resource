e6d24f36e600cab7738e90a75fc87e2984671328
minhlh <minhlh@haposoft.com>
2024-10-24 15:37:17 +0700

    feat: add craw

--- app/Jobs/CrawlDataLearningOutcomeJob.php
+++ app/Jobs/CrawlDataLearningOutcomeJob.php
@@ -1,24 +1,29 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\Jobs;
 
 use App\Services\LearningOutcome\CrawlDataLearningOutcomeService;
-use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
+
 class CrawlDataLearningOutcomeJob implements ShouldQueue
 {
-    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+    use Dispatchable;
+    use InteractsWithQueue;
+    use Queueable;
+    use SerializesModels;
 
     public function __construct(
         private readonly string $code,
-    )
-    {
+    ) {
     }
 
-    public function handle()
+    public function handle(): void
     {
         if ($this->code) {
             app(CrawlDataLearningOutcomeService::class)->crawlData($this->code);
