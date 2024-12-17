f5a470f4a7bb21a6af80da81a59784aa0aa85f17
minhlh <minhlh@haposoft.com>
2024-12-17 09:52:47 +0700

    feat: fix job

--- app/Jobs/UpdatePasswordStudentJob.php
+++ app/Jobs/UpdatePasswordStudentJob.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace App\Jobs;
 
+use App\Models\Student;
 use App\Supports\StudentHelper;
 use Exception;
 use Illuminate\Bus\Queueable;
@@ -38,17 +39,12 @@ class UpdatePasswordStudentJob implements ShouldQueue
         DB::beginTransaction();
         try {
 
-            DB::table('students')->orderBy('id')->chunk(100, function ($students): void {
-                $data = $students->map(function ($student) {
-                    return [
-                        'id' => $student->id,
-                        'password' => Hash::make($student->code),
-                        'email' => StudentHelper::makeEmailStudent($student->code)
-                    ];
-                });
-
-                DB::table('students')->upsert($data->toArray(), ['id'], ['password', 'email']);
-                Log::info('Update password student success 100 record');
+            Student::chunkById(100, function ($students): void {
+                foreach ($students as $student) {
+                    $student->password = Hash::make($student->code);
+                    $student->email = StudentHelper::makeEmailStudent($student->code);
+                    $student->save();
+                }
             });
             DB::commit();
             Log::info('Update password student success');
