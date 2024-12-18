5da7551e1dddd7ba064fb208ab73992788ff925c
minhlh <minhlh@haposoft.com>
2024-12-18 09:29:12 +0700

    feat: assigns class

---
+++ database/migrations/2024_12_17_115058_create_class_assigns_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('class_assigns', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('class_id');
+            $table->unsignedBigInteger('teacher_id');
+            $table->unsignedBigInteger('sub_teacher_id');
+            $table->string('number');
+            $table->integer('year');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('class_assigns');
+    }
+};
