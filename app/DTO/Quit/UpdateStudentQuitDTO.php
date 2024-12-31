27e54945c4190642d6eb6d50d753c84c30857a6e
minhlh <minhlh@haposoft.com>
2024-12-31 09:18:06 +0700

    fix: update quit

--- app/DTO/Quit/UpdateStudentQuitDTO.php
+++ app/DTO/Quit/UpdateStudentQuitDTO.php
@@ -8,9 +8,17 @@ class UpdateStudentQuitDTO
 {
     private int|string $id;
     private string $name;
-    private int $year;
-    private string $certification;
-    private string $certificationDate;
+    private ?int $year;
+    private ?string $certification;
+    private ?string $certificationDate;
+
+    public function __construct()
+    {
+        $this->name = "";
+        $this->year = null;
+        $this->certification = null;
+        $this->certificationDate = null;
+    }
 
     public function getId(): int
     {
@@ -33,32 +41,32 @@ class UpdateStudentQuitDTO
         $this->name = $name;
     }
 
-    public function getYear(): int
+    public function getYear(): ?int
     {
         return $this->year;
     }
 
-    public function setYear(int $year): void
+    public function setYear(?int $year): void
     {
         $this->year = $year;
     }
 
-    public function getCertification(): string
+    public function getCertification(): ?string
     {
         return $this->certification;
     }
 
-    public function setCertification(string $certification): void
+    public function setCertification(?string $certification): void
     {
         $this->certification = $certification;
     }
 
-    public function getCertificationDate(): string
+    public function getCertificationDate(): ?string
     {
         return $this->certificationDate;
     }
 
-    public function setCertificationDate(string $certificationDate): void
+    public function setCertificationDate(?string $certificationDate): void
     {
         $this->certificationDate = $certificationDate;
     }
